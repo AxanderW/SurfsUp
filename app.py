4821fa69ac259ed45dac65407c591577e0e7518d
John Doe <johndoe@users.noreply.github.com>
2018-11-26 16:55:21 -0500

    app.py file updated. start and start/end routes still need updating.

--- app.py
+++ app.py
@@ -49,28 +49,71 @@ app = Flask(__name__)
 
 # Flask routes
 @app.route("/")
+
+
 def welcome():
+
+    
     return(
         f"Thank you for visiting Precipitation API! <br>"
           "Get precipitation, temperature, elevation and a host of other data!<br>"
           "Planning a trip? Use this API for smart trip planning<br>"
           "Surfs Up, Brudha! <br>"
           "<br> Here are the available API Routess:<br>"
+          "/api/v1.0/precipitation <br>"
           "/api/v1.0/stations <br>"
-          "/api/v1.0/tobs<br>"
-          "/api/v1.0/<start>/<end>"
+          "/api/v1.0/tobs <br>"
+          "/api/v1.0/start_date <br>"
+          "/api/v1.0/start_date/end_date"
           
     )
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+       # Find lastest temp in dataset:
+    lastest_dt = session.query(Measurement.date).order_by(
+    Measurement.date.desc()).first()[0]
+
+    # Find previous year date
+    prev_year = (dt.datetime.strptime(lastest_dt, '%Y-%m-%d')
+             .date() - dt.timedelta(365)
+             ).strftime('%Y-%m-%d')
+   
+    
+
+    # Use most active station
+    most_active_station = session.query(Measurement.station, func.count(Measurement.station)).group_by(
+    Measurement.station).order_by(func.count(Measurement.station).desc()).first()[0]
+
+    # Query for date and temperature observations
+    prcps = session.query(Measurement.date, Measurement.prcp) .\
+    filter(Measurement.date.between(prev_year, lastest_dt)).\
+    filter(Measurement.station == most_active_station).all()
+
+    # Convert tobs to a dict
+    prcp_dict = {}
+
+    for rain in prcps:
+        prcp_dict[rain[0]] = rain[1]
+    
+    # Return date, temperatures in .json
+    return jsonify(prcp_dict)
 
 @app.route("/api/v1.0/stations")
+
 def stations ():
     """ Returns json list of all stations"""
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+
     stations = session.query(Station.station)\
         .all()
 
     return jsonify(stations)
 
 @app.route("/api/v1.0/tobs")
+
 def temperatures ():
     """  * query for the dates and temperature observations from a year from the last data point.
          * Return a JSON list of Temperature Observations (tobs) for the previous year. """
@@ -104,12 +147,26 @@ def temperatures ():
     # Return date, temperatures in .json
     return jsonify(tobs_dict)
 
-@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/start_date/<start_date>")
+
 def temperatures_by_start_date(start_date):
-    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-         filter(Measurement.date == start_date).all()
+
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+
+    tobs = session.query(Measurement.date, Measurement.tobs) .\
+    filter(Measurement.date == start_date)
+
+    return jsonify(tobs)
+
+    
+    ##    return "Date found"
+        #return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+         #filter(Measurement.date == start_date).all()
 
 @app.route("/api/v1.0/<start>/<end>")
+
 def temperatures_by_full_date(start_date, end_date):
     return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
          filter(Measurement.date >= start_date).filter(
