97169cbc69b13af25536ec5f70a188ff14b91ac1
John Doe <johndoe@users.noreply.github.com>
2018-11-26 20:10:52 -0500

    start_date and end_date routes updated. Program requirements met.

--- app.py
+++ app.py
@@ -1,31 +1,9 @@
-# Import dependecies 
-
-from flask import Flask, jsonify
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func, distinct
-import datetime as dt
-from datetime import date
-
-# SQL setup
-
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-
-# Save references to each table
-Measurement = Base.classes.measurement
-Station = Base.classes.station
-
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
-
-# Description of file rountines
+# Description app.py script
 """
-* Return the JSON representation of your dictionary.
+* `/api/v1.0/precipitation`
+    Convert the query results to a Dictionary using date as the key and prcp as the value.
 
+    Return the JSON representation of your dictionary.
 * `/api/v1.0/stations`
 
   * Return a JSON list of stations from the dataset.
@@ -43,6 +21,30 @@ session = Session(engine)
   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
 
 """
+# Import dependecies 
+
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, distinct
+import datetime as dt
+from datetime import date
+from sqlalchemy.pool import StaticPool
+import numpy as np
+
+# SQL setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread':False},
+    poolclass=StaticPool)
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
 # Flask setup
 app = Flask(__name__)
@@ -55,21 +57,26 @@ def welcome():
 
     
     return(
-        f"Thank you for visiting Precipitation API! <br>"
-          "Get precipitation, temperature, elevation and a host of other data!<br>"
-          "Planning a trip? Use this API for smart trip planning<br>"
-          "Surfs Up, Brudha! <br>"
-          "<br> Here are the available API Routess:<br>"
+        f"Thank you for visiting Precipitation API! "
+          "Get precipitation, temperature, elevation and a host of other data !<br>"
+          "Planning a trip? Use this API for smart trip planning. "
+          "Surfs Up, Brudha!<br>"
+          "<br>"
+          "Here are the available API Routes:<br>"
           "/api/v1.0/precipitation <br>"
           "/api/v1.0/stations <br>"
           "/api/v1.0/tobs <br>"
-          "/api/v1.0/start_date <br>"
-          "/api/v1.0/start_date/<start_date>/end_date/<end_date>"
+          "/api/v1.0/start_date/ <br>"         
+          "/api/v1.0/start_date/<start_date>/end_date/<end_date><br>"
+          "date format: YYYY-MM-DD <br>"
+          "sample query:/api/v1.0/start_date/2016-01-01 <br>"
+          "sample query: /api/v1.0/start_date/2016-01-01/end_date/2016-01-15 "
           
     )
 @app.route("/api/v1.0/precipitation")
 
 def precipitation():
+
        # Find lastest temp in dataset:
     lastest_dt = session.query(Measurement.date).order_by(
     Measurement.date.desc()).first()[0]
@@ -79,8 +86,6 @@ def precipitation():
              .date() - dt.timedelta(365)
              ).strftime('%Y-%m-%d')
    
-    
-
     # Use most active station
     most_active_station = session.query(Measurement.station, func.count(Measurement.station)).group_by(
     Measurement.station).order_by(func.count(Measurement.station).desc()).first()[0]
@@ -96,14 +101,13 @@ def precipitation():
 
 def stations ():
     """ Returns json list of all stations"""
-    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-    Base = automap_base()
-    Base.prepare(engine, reflect=True)
 
     stations = session.query(Station.station)\
         .all()
 
-    return jsonify(stations)
+    all_stations = list(np.ravel(stations))
+
+    return jsonify(all_stations)
 
 @app.route("/api/v1.0/tobs")
 
@@ -119,8 +123,6 @@ def temperatures ():
     prev_year = (dt.datetime.strptime(lastest_dt, '%Y-%m-%d')
              .date() - dt.timedelta(365)
              ).strftime('%Y-%m-%d')
-   
-    
 
     # Use most active station
     most_active_station = session.query(Measurement.station, func.count(Measurement.station)).group_by(
@@ -137,28 +139,27 @@ def temperatures ():
 
 def temperatures_by_start_date(start_date):
 
-    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-    Base = automap_base()
-    Base.prepare(engine, reflect=True)
- 
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-         filter(Measurement.date == start_date).all()
+         filter(Measurement.date >= start_date).all()
+    
+    all_temps = list(np.ravel(results))
 
-    return jsonify(results)
+    
+    return jsonify(all_temps)
+   
 
 @app.route("/api/v1.0/start_date/<start_date>/end_date/<end_date>")
 
 def temperatures_by_full_date(start_date, end_date):
 
-    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-    Base = automap_base()
-    Base.prepare(engine, reflect=True)
-
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
          filter(Measurement.date >= start_date).filter(
          Measurement.date <= end_date).all()
+
+    all_temps = list(np.ravel(results))
+
     
-    return jsonify(results)
+    return jsonify(all_temps)
 
 
 if __name__ == "__main__":
